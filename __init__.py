0ffbe2828a5d57ac0d0f464dfb8205373be01a29
Kudzai Tsapo <kudzaitsapo@gmail.com>
2018-11-08 12:57:13 +0200

    Initialization of outside files

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""
+@author: Kudzai Tsapo (kudzai@charteredsys.com)
+
+Description: 
+--------------
+This module abstracts the execution of stored procedures and sql queries as well as fetching of rows from an existing 
+SQL Server database. 
+
+Dependencies:
+--------------
+pyodbc,
+ODBC Driver 13 for SQL Server
+
+"""
+
+from models import *
---
+++ test.py
@@ -0,0 +1,13 @@
+"""
+@author: Kudzai Tsapo (kudzai@charteredsys.com)
+
+Description
+-----------
+File for testing methods implemented in the library
+
+"""
+
+from models import Model
+
+sql_string = open('sql/testfile.sql', 'r').read()
+print(sql_string)
