f1bc3fd388c2a106e59b26006ba0851d3c305b91
Kudzai Tsapo <kudzaitsapo@gmail.com>
2018-11-08 12:58:44 +0200

    Initializing models files

---
+++ models/orm.py
@@ -0,0 +1,10 @@
+"""
+@author: Kudzai Tsapo (kudzai@charteredsys.com)
+
+Description: 
+--------------
+This file contains the model classes for the Object Relational Mapper (Custom made)
+"""
+
+class Field:
+    def __set__(self, )
