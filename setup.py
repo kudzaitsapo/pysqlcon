718ac42a1944d2d0e5b4af4a1fcafbf0bee4a567
Kudzai <kudzaitsapo@gmail.com>
2019-03-07 15:43:45 +0200

    Added autocommit to the connection

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
  
 setup(name='',
-      version='0.1',
-      url='https://github.com/the-gigi/pathology',
+      version='0.4',
+      url='https://github.com/kudzaitsapo/pysqlcon',
       license='MIT',
-      author='Gigi Sayfan',
-      author_email='the.gigi@gmail.com',
-      description='Add static script_dir() method to Path',
+      author='Kudzai Tsapo',
+      author_email='kudzai@charteredsys.com',
+      description='Easy execution of sql statements and stored procedures on MSSQL database',
       packages=find_packages(exclude=['tests']),
       long_description=open('README.md').read(),
       zip_safe=False)
